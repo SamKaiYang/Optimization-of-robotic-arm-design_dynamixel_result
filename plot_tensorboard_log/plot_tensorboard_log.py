61e4e36444ba5bc220d79c84e61efc2f86e07e77
SamKaiYang <tt00621212@gmail.com>
2023-05-14 17:12:50 +0800

    fixed add origin test

--- plot_tensorboard_log/plot_tensorboard_log.py
+++ plot_tensorboard_log/plot_tensorboard_log.py
@@ -72,7 +72,9 @@ from tensorboard.backend.event_processing.event_accumulator import EventAccumula
 import matplotlib.pyplot as plt
 
 # 指定 event 文件的路徑
-event_files = ["./Mar18_12-50-25_ws2030/events.out.tfevents.1679140225.ws2030", "./Mar29_14-29-40_ws2020/events.out.tfevents.1680092980.ws2020", "./Apr02_13-15-21_ws2030/events.out.tfevents.1680434121.ws2030"]
+event_files = ["./Mar18_12-50-25_ws2030/events.out.tfevents.1679140225.ws2030", \
+               "./May12_15-23-09_d84979fad1fc_DDQN/events.out.tfevents.1683897789.d84979fad1fc", \
+               "./May12_15-18-04_ws2030_C51/events.out.tfevents.1683897484.ws2030"]
 
 # 設定圖表的樣式
 plt_themes = ["seaborn-darkgrid", "ggplot", "dark_background", "bmh", "fivethirtyeight"]
@@ -132,4 +134,61 @@ for i, event_file in enumerate(event_files):
 # 顯示圖表
 plt.savefig('plot_tensorborad_log.png', dpi=300)
 plt.show()
+# '''
+
+# 個別plot
+# 從每個 event 文件中讀取數據，並繪製在對應的子圖上
+for i, event_file in enumerate(event_files):
+    # 建立一個 EventAccumulator 對象來讀取 event 文件
+    event_acc = EventAccumulator(event_file)
+    event_acc.Reload()
+
+    # 取得所有 summary keys
+    tags = event_acc.Tags()["scalars"]
+    color = ['red', 'green', 'blue']
+    linestyle = ['dashed', 'dotted', 'dashdot']
+    Log = ['DQN','DDQN','C51']
+
+    # 建立一個 Figure 物件和多個 Axes 物件
+    fig, axs = plt.subplots(3, 2, figsize=(10, 10))
+    # 從 event 文件中讀取數據
+    for tag in tags:
+        events = event_acc.Scalars(tag)
+        steps = [event.step for event in events]
+        value = [event.value for event in events]
+        
+        # 繪製在對應的子圖上
+        if tag == "trained-model/Loss_per_frame/":
+            axs[0, 0].plot(steps, value, label=Log[i], color=color[i], marker='o', linestyle=linestyle[i], linewidth=0.5, markersize=0.5)
+            axs[0, 0].set_xlabel("Step")
+            axs[0, 0].set_ylabel("Loss")
+            axs[0, 0].legend(bbox_to_anchor=(1.05, 1), loc='upper right')
+        elif tag == "trained-model/train_step_reward/":
+            axs[0, 1].plot(steps, value, label=Log[i], color=color[i], marker='o', linestyle=linestyle[i], linewidth=0.5, markersize=0.5)
+            axs[0, 1].set_xlabel("Step")
+            axs[0, 1].set_ylabel("Reward")
+            axs[0, 1].legend(bbox_to_anchor=(1.05, 1), loc='upper right')
+        elif tag == "trained-model/loss_log/":
+            axs[1, 0].plot(steps, value, label=Log[i], color=color[i], marker='o', linestyle=linestyle[i], linewidth=0.5, markersize=0.5)
+            axs[1, 0].set_xlabel("Step")
+            axs[1, 0].set_ylabel("Loss")
+            axs[1, 0].legend(bbox_to_anchor=(1.05, 1), loc='upper right')
+        elif tag == "trained-model/Average_Return/":
+            axs[1, 1].plot(steps, value, label=Log[i], color=color[i], marker='o', linestyle=linestyle[i], linewidth=0.5, markersize=0.5)
+            axs[1, 1].set_xlabel("Step")
+            axs[1, 1].set_ylabel("Reward")
+            axs[1, 1].legend(bbox_to_anchor=(1.05, 1), loc='upper right')
+        elif tag == "tested-model/test_step_reward/":
+            axs[2, 0].plot(steps, value, label=Log[i], color=color[i], marker='o', linestyle=linestyle[i], linewidth=0.5, markersize=0.5)
+            axs[2, 0].set_xlabel("Step")
+            axs[2, 0].set_ylabel("Reward")
+            axs[2, 0].legend(bbox_to_anchor=(1.05, 1), loc='upper right')
+        elif tag == "tested-model/test_episode_reward/":
+            axs[2, 1].plot(steps, value, label=Log[i], color=color[i], marker='o', linestyle=linestyle[i], linewidth=0.5, markersize=0.5)
+            axs[2, 1].set_xlabel("Episode")
+            axs[2, 1].set_ylabel("Reward")
+            axs[2, 1].legend(bbox_to_anchor=(1.05, 1), loc='upper right')
+    # 顯示圖表
+    plt.savefig('plot_tensorborad_log_'+Log[i]+'.png', dpi=300)
+    plt.show()
 # '''
--- print_best_result_excel/print_best_result_excel_for_case2.py
+++ print_best_result_excel/print_best_result_excel_for_case2.py
@@ -8,12 +8,18 @@ import os
                 #    names=['ratio', 'torque', 'consuption', 'reachable', 'manipulator', 'axis2', 'axis3', 'all_length',\
                 #            'nan', 'reward', 'nan', 'motor2', 'motor3'])
 # 設定要讀取的檔案路徑和開頭字串
-file_path = './test_result_excel'
+file_path = './ddqn_tested_reward_state'
 file_prefix = 'tested_reward_state_'  # 或者是其他開頭字串 # 1-A-10
 
 # 獲取符合開頭字串的所有檔案路徑
 file_list = [os.path.join(file_path, f) for f in os.listdir(file_path) if f.startswith(file_prefix)]
 print('file_list:',file_list)
+
+# Use list comprehension to extract the parts from the file names
+parts_list = [file_name.split('/')[-1].split('.')[0].split('_')[-1] for file_name in file_list]
+
+print(parts_list)
+
 df_list = [pd.read_excel(f,header=None, \
                    names=['ratio', 'torque', 'consuption', 'reachable', 'manipulator', 'axis2', 'axis3', 'all_length',\
                            'nan', 'reward', 'nan', 'motor2', 'motor3']) for f in file_list]
@@ -21,6 +27,7 @@ df_list = [pd.read_excel(f,header=None, \
 for _ in range(len(file_list)):
         df_sorted = df_list[_].sort_values(by=['reachable', 'manipulator', 'consuption'], ascending=[False, False, True])
         print('file_list:',file_list[_])
+        print('parts_list:',parts_list[_])
         # 打印排序后的DataFrame，以确认最佳值
         print(df_sorted)
 
@@ -32,6 +39,38 @@ for _ in range(len(file_list)):
         best_torque = df_sorted.iloc[0]['torque']
         
         print(df_sorted.iloc[0].transpose())
+
+        print("------最佳配置-----")
+        best_axis2 = df_sorted.iloc[0]['axis2']
+        best_torque = df_sorted.iloc[0]['axis3']
+        best_motor2 = df_sorted.iloc[0]['motor2']
+        best_motor3 = df_sorted.iloc[0]['motor3']
         # print(best_reachable)
         # print(best_manipulator)
         # print(best_power_consumption)
+
+        # 與原設計比較
+        f_ori = "tested_state_original_design.xlsx"
+        ori_df = pd.read_excel(f_ori,header=None, \
+                        names=['mission_name', 'ratio','torque', 'consuption', 'reachable', 'manipulator', 'axis2', 'axis3',\
+                                'motor2', 'motor3'])
+        # 搜尋特定欄位中的值 mission_name# 選擇第一列
+        mission_name_row = ori_df.iloc[:,0]
+
+        # 搜尋第一列中特定的值
+        search_value = parts_list[_]
+        if search_value in mission_name_row.values:
+                # 印出特定值所在的欄位位置
+                index = list(mission_name_row.values).index(search_value)
+                print(f'The search value "{search_value}" was found in column {index + 1}.')
+                # mission_ori_result = ori_df.iloc[index]
+                # 访问值的行和列
+                ori_reachable = ori_df.iloc[index]['reachable']
+                ori_manipulator = ori_df.iloc[index]['manipulator']
+                ori_power_consumption = ori_df.iloc[index]['consuption']
+                ori_ratio = ori_df.iloc[index]['ratio']
+                ori_torque = ori_df.iloc[index]['torque']
+                print(ori_df.iloc[index].transpose())
+                # print("mission_ori_result:",mission_ori_result)
+        else:
+                print(f'The search value "{search_value}" was not found in the first row.')
